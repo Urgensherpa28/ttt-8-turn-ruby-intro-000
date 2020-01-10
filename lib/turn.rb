39152b63d7196a2c14c44e6d3cb89199bb3d67f4
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-10 17:37:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   index = gets
-  if index != valid_move?(board, index)
+  if index == valid_move?(board, index)
     input_to_index(input)
     #  board = input_to_index(input)
    end
