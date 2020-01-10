0bb7f3743de4a13d6b837e09278903c78e01d9aa
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-10 16:57:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,11 +32,5 @@ def move(board, index, player = "X")
 end
 
 def turn(board)
-  input_to_index = puts "Please enter 1-9:"
-  number = gets
-    if valid_move?(number)
-      move
-    else
-      ask
-    end
+  puts "Please enter 1-9:"
 end
