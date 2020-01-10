ac79ceee69fd4796106427c024aafa9ad6585616
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-10 18:49:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if index == valid_move?(board, index)
-    true
+    move(board, index, player = "X")
   else
     turn(board)
   end
