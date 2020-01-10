6daf6ea68098458df267284eb318f59b64b49b14
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-10 18:22:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, 0) && index.between?(0, 8)
+  if position_taken?(board, index) && index.between?(0, 8)
     true
   else
     false
